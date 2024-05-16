48452266ed81ff4227a0217a1cd2ffe82ddfdb85
Herbert Spencer <hspencer@ead.cl>
2024-05-16 13:32:54 -0400

    URLs

--- importer.js
+++ importer.js
@@ -4,7 +4,8 @@
  */
 
 // URL de la API para obtener los casos de estudio específicos sobre patrones oscuros.
-let queryUrl = "https://wiki.ead.pucv.cl/api.php?action=ask&format=json&query=%5B%5BCategor%C3%ADa%3ACaso%20de%20Estudio%5D%5D%5B%5BPalabras%20Clave%3A%3Adark%5D%5D%20%7C%3FNombre%20%7C%3FURL%20%7C%3FDescripci%C3%B3n%20Corta%20%7C%3FDescripci%C3%B3n%20%7C%3FPalabras%20Clave%20%7C%3FImgPath%20%7C%3FImgPaths";
+let queryUrl =
+  "https://wiki.ead.pucv.cl/api.php?action=ask&format=json&query=%5B%5BCategor%C3%ADa%3ACaso%20de%20Estudio%5D%5D%5B%5BPalabras%20Clave%3A%3Adark%5D%5D%20%7C%3FNombre%20%7C%3FURL%20%7C%3FDescripci%C3%B3n%20Corta%20%7C%3FDescripci%C3%B3n%20%7C%3FPalabras%20Clave%20%7C%3FImgPath%20%7C%3FImgPaths";
 let data; // Variable para almacenar los datos obtenidos de la API.
 let casos = []; // Arreglo para almacenar los objetos de cada caso de estudio.
 
@@ -30,14 +31,33 @@ function constructObjects() {
   for (let key in data.query.results) {
     let caso = data.query.results[key];
     let name = caso.printouts["Nombre"]?.join(", ") ?? "Nombre no disponible";
-    let shortDesc = caso.printouts["Descripción Corta"]?.join(", ") ?? "Descripción corta no disponible";
-    let desc = caso.printouts["Descripción"]?.join(", ") ?? "Descripción no disponible";
-    let keywords = caso.printouts["Palabras Clave"]?.join(", ") ?? "Palabras clave no disponibles";
-    let img = caso.printouts["ImgPath"]?.join(", ") ?? "https://loremflickr.com/640/360";
-    let imgs = caso.printouts["ImgPaths"]?.join(", ") ?? "https://loremflickr.com/640/360";
+    let shortDesc =
+      caso.printouts["Descripción Corta"]?.join(", ") ??
+      "Descripción corta no disponible";
+    let desc =
+      caso.printouts["Descripción"]?.join(", ") ?? "Descripción no disponible";
+    let keywords =
+      caso.printouts["Palabras Clave"]?.join(", ") ??
+      "Palabras clave no disponibles";
+    let img =
+      caso.printouts["ImgPath"]?.join(", ") ??
+      "https://loremflickr.com/640/360";
+    let imgs =
+      caso.printouts["ImgPaths"]?.join(", ") ??
+      "https://loremflickr.com/640/360";
     let url = caso.printouts["URL"]?.join(", ") ?? "URL no disponible";
     let id = toCanonical(name); // Convertir nombre a formato canónico.
-    let casoNuevo = new Caso(name, id, shortDesc, desc, keywords, url, caso.fullurl, img, imgs);
+    let casoNuevo = new Caso(
+      name,
+      id,
+      shortDesc,
+      desc,
+      keywords,
+      url,
+      caso.fullurl,
+      img,
+      imgs
+    );
     casos.push(casoNuevo);
   }
   constructSelector(); // Construir el selector después de mostrar todos los casos.
@@ -45,7 +65,17 @@ function constructObjects() {
 
 // Clase para representar un caso de estudio.
 class Caso {
-  constructor(name, id, shortDesc, desc, keywords, url, casiopeaUrl, img, imgs) {
+  constructor(
+    name,
+    id,
+    shortDesc,
+    desc,
+    keywords,
+    url,
+    casiopeaUrl,
+    img,
+    imgs
+  ) {
     this.name = name;
     this.id = id;
     this.shortDesc = shortDesc;
@@ -59,86 +89,107 @@ class Caso {
 
   // Método para mostrar un caso en formato reducido.
   displaySmall() {
-    let currentUrl = window.location.href.split('?')[0];
+    let currentUrl = window.location.href.split("?")[0];
     let urlWithParam = `${currentUrl}?id=${this.id}`;
     let casoContainer = createDiv(
-      "<a href='#' onclick='event.preventDefault(); goToURL(\"" + urlWithParam + "\", \"" + this.id + "\");'>" +
-      "<img src='" + this.img + "' title='" + this.name + "' />" +
-      "<div class='overlay'>" +
-      "<h4>" + this.name + "</h4>" +
-      "<div class='desc'>" + this.shortDesc + "</div></div></a>"
+      "<a href='#' onclick='event.preventDefault(); goToURL(\"" +
+        urlWithParam +
+        '", "' +
+        this.id +
+        "\");'>" +
+        "<img src='" +
+        this.img +
+        "' title='" +
+        this.name +
+        "' />" +
+        "<div class='overlay'>" +
+        "<h4>" +
+        this.name +
+        "</h4>" +
+        "<div class='desc'>" +
+        this.shortDesc +
+        "</div></div></a>"
     );
-    casoContainer.class('caso');
-    casoContainer.parent('main');
+    casoContainer.class("caso");
+    casoContainer.parent("main");
   }
 
-// Método para mostrar detalles completos de un caso.
-display() {
-  clearAll(); // Limpia todos los contenidos antes de mostrar nuevos datos.
+  // Método para mostrar detalles completos de un caso.
+  display() {
+    clearAll(); // Limpia todos los contenidos antes de mostrar nuevos datos.
 
-  // Agregar el título
-  let htmlTitle = document.getElementById('title');
-  htmlTitle.innerHTML = this.name;
+    // Agregar el título
+    let htmlTitle = document.getElementById("title");
+    htmlTitle.innerHTML = this.name;
 
-  // Comienza a construir el contenido HTML
-  let htmlContent = `<img src='${this.img}' title='${this.name}' />` +
-                    `<div class='desc'>${this.desc}<br><br><strong>Palabras Clave</strong>: ${this.keywords}<br><br>`;
+    // Comienza a construir el contenido HTML (en este caso, son 2 columnas)
+    let htmlContent =
+      `<img src='${this.img}' title='${this.name}' />` +
+      `<div class='desc'>${this.desc}<br><br><strong>Palabras Clave</strong>: ${this.keywords}<br><br>`;
 
-// Agregar enlaces de URLs
-const urls = this.url.split(", "); // Divide las URLs independientemente de si hay una o varias
-let urlsHtml = ''; // Inicializa un string vacío para construir el HTML de enlaces.
+    // Agregar enlaces de URLs
+    const urls = this.url.split(", "); // Divide las URLs independientemente de si hay una o varias
+    let urlsHtml = "<strong>Este patrón se usa en:</strong><br>"; // Inicializa un string vacío para construir el HTML de enlaces.
 
-urls.forEach(url => {
-  urlsHtml += `<a href='${url.trim()}'>${url.trim()}</a><br>`; // Agrega cada URL como un enlace.
-});
+    urls.forEach((url) => {
+      urlsHtml += `<a href='${url.trim()}'>${url.trim()}</a><br>`; // Agrega cada URL como un enlace.
+    });
 
-// Agregar imágenes extra dentro de un div.gallery si hay imágenes disponibles
-let imgsHtml = ''; // Inicializa un string vacío para construir el HTML de imágenes.
+    htmlContent += urlsHtml; // le sumo las URL al HTML que estoy componientdo
 
-if (this.imgs && this.imgs.trim() !== "") { // Verifica que la variable imgs contiene algo antes de proceder
-  const imgsArray = this.imgs.split(", "); // Divide las imágenes en un array
-  imgsArray.forEach(img => {
-    imgsHtml += `<img src='${img.trim()}' alt='Imagen adicional para ${this.name}' />`;
-  });
+    // Agregar imágenes extra dentro de un div.gallery si hay imágenes disponibles
+    let imgsHtml = ""; // Inicializa un string vacío para construir el HTML de imágenes.
 
-  // Si imgsHtml tiene contenido después de procesar las imágenes, envolver en div.gallery
-  if (imgsHtml) {
-    imgsHtml = `<div class='gallery'>${imgsHtml}</div>`; // Envuelve las imágenes en un contenedor gallery si hay alguna imagen
+    if (this.imgs && this.imgs.trim() !== "") {
+      // Verifica que la variable imgs contiene algo antes de proceder
+      const imgsArray = this.imgs.split(", "); // Divide las imágenes en un array
+      imgsArray.forEach((img) => {
+        imgsHtml += `<img src='${img.trim()}' alt='Imagen adicional para ${
+          this.name
+        }' />`;
+      });
+
+      // Si imgsHtml tiene contenido después de procesar las imágenes, envolver en div.gallery
+      if (imgsHtml) {
+        imgsHtml = `<div class='gallery'>${imgsHtml}</div>`; // Envuelve las imágenes en un contenedor gallery si hay alguna imagen
+      }
+    }
+    // Agregar el HTML de imágenes al contenido principal, solo si imgsHtml no está vacío
+    if (imgsHtml) {
+      htmlContent += imgsHtml; // Agrega el contenedor gallery solo si hay imágenes
+    }
+
+    // Continuar construyendo el contenido HTML
+    htmlContent += `<a class='wiki-link' href='${this.casiopeaURL}'>Link a Casiopea</a></div>`;
+
+    // Asignar el contenido HTML al contenedor y ajustar la clase
+    let singleContainer = createDiv(htmlContent);
+    singleContainer.class("single");
+    singleContainer.parent(document.getElementById("single"));
   }
-}
-// Agregar el HTML de imágenes al contenido principal, solo si imgsHtml no está vacío
-if (imgsHtml) {
-  htmlContent += imgsHtml; // Agrega el contenedor gallery solo si hay imágenes
-}
-
-  // Continuar construyendo el contenido HTML
-  htmlContent += `<a class='wiki-link' href='${this.casiopeaURL}'>Link a Casiopea</a></div>`;
-
-  // Asignar el contenido HTML al contenedor y ajustar la clase
-  let singleContainer = createDiv(htmlContent);
-  singleContainer.class('single');
-  singleContainer.parent(document.getElementById('single'));
-}
 }
 
 // Función para generar un selector y permitir la navegación entre casos.
 function constructSelector() {
   let sel = createSelect();
   sel.option("Selecciona un patrón oscuro");
-  casos.forEach(caso => {
+  casos.forEach((caso) => {
     sel.option(caso.name, caso.id);
   });
-  sel.parent('footerSelect');
+  sel.parent("footerSelect");
   sel.changed(() => {
     let selectedId = sel.value();
-    goToURL(window.location.href.split('?')[0] + "?id=" + selectedId, selectedId);
+    goToURL(
+      window.location.href.split("?")[0] + "?id=" + selectedId,
+      selectedId
+    );
   });
 }
 
 // Función para mostrar todos los casos en formato pequeño.
 function displayAll() {
   clearAll();
-  let htmlTitle = document.getElementById('title');
+  let htmlTitle = document.getElementById("title");
   htmlTitle.innerHTML = "Patrones Oscuros del Diseño de Interacción";
   for (let caso of casos) {
     caso.displaySmall();
@@ -147,9 +198,9 @@ function displayAll() {
 
 // Funciones adicionales para manejar la visualización y navegación de casos.
 function displayCase(id) {
-  let mainContainer = document.querySelector('main');
-  mainContainer.innerHTML = '';
-  let selectedCaso = casos.find(caso => caso.id === id);
+  let mainContainer = document.querySelector("main");
+  mainContainer.innerHTML = "";
+  let selectedCaso = casos.find((caso) => caso.id === id);
   if (selectedCaso) {
     document.title = selectedCaso.name + " - Dark Patterns";
     selectedCaso.display();
@@ -158,12 +209,12 @@ function displayCase(id) {
 
 function goToURL(url, id = null) {
   if (id) {
-    history.pushState({id: id}, '', url);
+    history.pushState({ id: id }, "", url);
     displayCase(id);
   } else {
     const selectedId = sel.value();
-    const newUrl = window.location.href.split('?')[0] + "?id=" + selectedId;
-    history.pushState({id: selectedId}, '', newUrl);
+    const newUrl = window.location.href.split("?")[0] + "?id=" + selectedId;
+    history.pushState({ id: selectedId }, "", newUrl);
     displayCase(selectedId);
   }
 }
@@ -171,17 +222,17 @@ function goToURL(url, id = null) {
 // Convertir nombres a permalinks, facilitando URLs amigables.
 function toCanonical(name) {
   let canonical = name.toLowerCase();
-  const accentMap = {á: 'a', é: 'e', í: 'i', ó: 'o', ú: 'u', ü: 'u', ñ: 'n'};
-  canonical = canonical.replace(/[áéíóúüñ]/g, match => accentMap[match]);
-  canonical = canonical.replace(/\s+/g, '-');
-  canonical = canonical.replace(/[^a-z0-9-]/g, '');
+  const accentMap = { á: "a", é: "e", í: "i", ó: "o", ú: "u", ü: "u", ñ: "n" };
+  canonical = canonical.replace(/[áéíóúüñ]/g, (match) => accentMap[match]);
+  canonical = canonical.replace(/\s+/g, "-");
+  canonical = canonical.replace(/[^a-z0-9-]/g, "");
   return canonical;
 }
 
 // Limpiar todos los contenidos antes de cargar nuevos casos.
 function clearAll() {
-  let htmlTitle = document.getElementById('title');
-  htmlTitle.innerHTML = ''; // Limpiar el título del caso en la página.
-  let articleContainer = document.getElementById('single');
-  articleContainer.innerHTML = ''; // Limpiar el contenido del artículo antes de añadir nuevo contenido.
+  let htmlTitle = document.getElementById("title");
+  htmlTitle.innerHTML = ""; // Limpiar el título del caso en la página.
+  let articleContainer = document.getElementById("single");
+  articleContainer.innerHTML = ""; // Limpiar el contenido del artículo antes de añadir nuevo contenido.
 }
